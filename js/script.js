95b9bf21875428bba8edcb64ff71cb736eac6111
Wojciech Leksza <wojtek.leksza@gmail.com>
2022-08-31 13:14:44 +0200

    added js functions

--- js/script.js
+++ js/script.js
@@ -1,41 +1,53 @@
-console.log("Witaj sprytny programisto!")
+{
+    const picture = document.querySelector(".js-picture");
+    const nextButton = document.querySelector(".js-nextButton");
+    const previousButton = document.querySelector(".js-previousButton");
 
-let previousButton = document.querySelector(".js-previousButton");
-let nextButton = document.querySelector(".js-nextButton");
-let picture = document.querySelector(".js-picture");
-
-nextButton.addEventListener("click", () => {
-    switch (picture.getAttribute("src")) {
-        case "images/my_picture_1.jpg":
-            picture.setAttribute("src", "images/my_picture_2.jpg");
-            previousButton.classList.remove("hide");
-            nextButton.classList.remove("hide");
-            break;
-        case "images/my_picture_2.jpg":
-            picture.setAttribute("src", "images/my_picture_3.jpg");
-            previousButton.classList.remove("hide");
-            nextButton.classList.add("hide");
-            break;
-        default:
-            previousButton.classList.remove("hide");
-            nextButton.classList.add("hide");
+    const welcome = () => {
+        console.log("Witaj sprytny programisto!");
     }
-})
 
-previousButton.addEventListener("click", () => {
-    switch (picture.getAttribute("src")) {
-        case "images/my_picture_2.jpg":
-            picture.setAttribute("src", "images/my_picture_1.jpg");
-            previousButton.classList.add("hide");
-            nextButton.classList.remove("hide");
-            break;
-        case "images/my_picture_3.jpg":
-            picture.setAttribute("src", "images/my_picture_2.jpg");
-            previousButton.classList.remove("hide");
-            nextButton.classList.remove("hide");
-            break;
-        default:
-            previousButton.classList.add("hide");
-            nextButton.classList.remove("hide");
+    const onNextButtonClick = () => {
+        nextButton.addEventListener("click", () => {
+            switch (picture.getAttribute("src")) {
+                case "images/my_picture_1.jpg":
+                    picture.setAttribute("src", "images/my_picture_2.jpg");
+                    previousButton.classList.remove("hide");
+                    nextButton.classList.remove("hide");
+                    break;
+                case "images/my_picture_2.jpg":
+                    picture.setAttribute("src", "images/my_picture_3.jpg");
+                    previousButton.classList.remove("hide");
+                    nextButton.classList.add("hide");
+                    break;
+                default:
+                    previousButton.classList.remove("hide");
+                    nextButton.classList.add("hide");
+            }
+        })
     }
-})
+
+    const onPreviousButtonClick = () => {
+        previousButton.addEventListener("click", () => {
+            switch (picture.getAttribute("src")) {
+                case "images/my_picture_2.jpg":
+                    picture.setAttribute("src", "images/my_picture_1.jpg");
+                    previousButton.classList.add("hide");
+                    nextButton.classList.remove("hide");
+                    break;
+                case "images/my_picture_3.jpg":
+                    picture.setAttribute("src", "images/my_picture_2.jpg");
+                    previousButton.classList.remove("hide");
+                    nextButton.classList.remove("hide");
+                    break;
+                default:
+                    previousButton.classList.add("hide");
+                    nextButton.classList.remove("hide");
+            }
+        })
+    }
+
+    welcome();
+    onNextButtonClick();
+    onPreviousButtonClick();
+}
